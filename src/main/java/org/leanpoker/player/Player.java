067e010c6858b575d3f2f9335a5a06d26036fb14
cickib <bellakocsis06@gmail.com>
2017-01-12 12:05:39 +0100

    .

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -6,21 +6,17 @@ import com.google.gson.JsonObject;
 
 public class Player {
 
-    static final String VERSION = "_ZB.ZS";
+    static final String VERSION = "_ZB.ZS rulez";
 
     public static int betRequest(JsonElement request) {
         JsonObject json = request.getAsJsonObject();
-        System.out.println("find me " + json);
         String ownStack = "0";
         int bet = 0;
         JsonArray holeCards = new JsonArray();
         String playerStatus;
         JsonArray playerList = json.get("players").getAsJsonArray();
         for (JsonElement player : playerList) {
-            System.out.println("player " + player);
-            System.out.println("zbzs "+ player.getAsJsonObject().get("name"));
             if (player.getAsJsonObject().get("name").toString().equals("_ZB.ZS")) {
-                System.out.println("player zbzs " + player.getAsJsonObject().get("name").toString());
                 ownStack = player.getAsJsonObject().get("stack").toString();
                 holeCards = player.getAsJsonObject().get("hole_cards").getAsJsonArray();
             }
@@ -29,19 +25,17 @@ public class Player {
                 bet = player.getAsJsonObject().get("bet").getAsInt();
             }
         }
-        int currentBuyIn = json.get("current_buy_in").getAsInt();
-        System.out.println("find me 2 " + json);
+        Integer currentBuyIn = json.get("current_buy_in").getAsInt();
 
         if (holeCards.size() == 2) {
             if (currentBuyIn > 0) {
                 JsonElement firstCard = holeCards.get(0).getAsJsonObject().get("rank");
                 JsonElement secondCard = holeCards.get(1).getAsJsonObject().get("rank");
-                System.out.println("find me " + firstCard + " " + secondCard);
                 if (firstCard.equals(secondCard) || (firstCard.equals("A") && secondCard.getAsInt() > 8) || (secondCard.equals("A") && firstCard.getAsInt() > 8)
                         || (firstCard.equals("K") && secondCard.getAsInt() > 9) || (secondCard.equals("K") && firstCard.getAsInt() > 9) ||
                         (firstCard.equals("Q") && secondCard.equals("J")) || (secondCard.equals("Q") && firstCard.equals("J")) ||
                         (firstCard.equals("J") && ((secondCard.equals(10) && holeCards.get(1).getAsJsonObject().get("suit").equals(holeCards.get(0).getAsJsonObject().get("suit").getAsJsonObject()))))) {
-
+                    System.out.println("zbzs action if good cards " + (currentBuyIn - bet + json.get("minimum_raise").getAsInt()));
                     return currentBuyIn - bet + json.get("minimum_raise").getAsInt();
                 }
             }
