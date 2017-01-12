2140e45e96770b2f81c56a72146e20b3b149c8e2
cickib <bellakocsis06@gmail.com>
2017-01-12 10:12:20 +0100

    printing stuff

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,6 +9,7 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
+        System.out.println(request.getAsJsonObject().get("current_buy_in"));
         return 1;
     }
 
