b8c38d78dad166870a3e709f658f0f9faaffcb21
cickib <bellakocsis06@gmail.com>
2017-01-12 13:10:10 +0100

    refactored Player

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,36 +9,59 @@ public class Player {
     static final String VERSION = "_ZB.ZS rulez";
 
     public static int betRequest(JsonElement request) {
+
         JsonObject json = request.getAsJsonObject();
-        String ownStack = "0";
+
+        System.out.println("find json " + json);
+
+        JsonArray community_cards = json.get("community_cards").getAsJsonArray();
+        int betIndex = json.get("bet_index").getAsInt();
+        Integer currentBuyIn = json.get("current_buy_in").getAsInt();
+        Integer smallBlind = json.get("small_blind").getAsInt();
+        Integer dealer = json.get("dealer").getAsInt();
+        Integer minimumRaise = json.get("minimum_raise").getAsInt();
+        Integer pot = json.get("pot").getAsInt();
         int bet = 0;
-        JsonArray holeCards = new JsonArray();
-        String playerStatus;
+        String playerStatus = "in_action";
+
         JsonArray playerList = json.get("players").getAsJsonArray();
+
+        Integer ownStack = 0;
+        JsonArray holeCards = new JsonArray();
+
         for (JsonElement player : playerList) {
             if (player.getAsJsonObject().get("name").toString().equals("_ZB.ZS")) {
-                ownStack = player.getAsJsonObject().get("stack").toString();
+                ownStack = player.getAsJsonObject().get("stack").getAsInt();
                 holeCards = player.getAsJsonObject().get("hole_cards").getAsJsonArray();
-            }
-            playerStatus = player.getAsJsonObject().get("status").toString();
-            if (playerStatus.equals("in_action")) {
                 bet = player.getAsJsonObject().get("bet").getAsInt();
             }
+//            if (playerStatus.equals(player.getAsJsonObject().get("status").getAsString())) {
+//                bet = player.getAsJsonObject().get("bet").getAsInt();
+//            }
         }
-        Integer currentBuyIn = json.get("current_buy_in").getAsInt();
-
-        Integer smallBlind = json.get("small_blind").getAsInt();
+        System.out.println("find holeCards.size() " + holeCards.size());
         if (holeCards.size() == 2) {
-            if (currentBuyIn > smallBlind*2) {
+            if (currentBuyIn > smallBlind * 2) {
                 Cards cards = new Cards(holeCards.get(0).getAsJsonObject(), holeCards.get(1).getAsJsonObject());
                 if (cards.hasAceAndNine() || cards.hasJockAndQueen() || cards.hasKingAndTen() || cards.hasPair() || cards.hasSameColorEightAndHigher()) {
-                    System.out.println("zbzs action if good cards " + (currentBuyIn - bet + json.get("minimum_raise").getAsInt()));
-                    return Integer.parseInt(ownStack);
-//                    return currentBuyIn - bet + json.get("minimum_raise").getAsInt();
+                    System.out.println("find allin " + ownStack);
+                    if (ownStack > 0) {
+                        return ownStack;
+                    }
                 }
             }
         }
         return 0;
+
+// Actions to use:
+//        raise:
+//              return currentBuyIn - bet + minimumRaise;
+//        call:
+//              return currentBuyIn - bet;
+//        fold:
+//              return 0;
+//        allin:
+//              return Integer.parseInt(ownStack);
 
     }
 
