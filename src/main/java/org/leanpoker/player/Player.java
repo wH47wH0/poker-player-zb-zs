8ece23ee0fa9894eee0612069c02836036056471
cickib <bellakocsis06@gmail.com>
2017-01-12 10:28:00 +0100

    raising?

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,7 +1,9 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import jdk.nashorn.api.scripting.JSObject;
 
 public class Player {
 
@@ -9,8 +11,16 @@ public class Player {
 
     public static int betRequest(JsonElement request) {
         JsonObject json = request.getAsJsonObject();
+        int bet=0;
+        JsonArray jsonArray = json.get("players").getAsJsonArray();
+        for (JsonElement array : jsonArray){
+            String playerStatus = array.getAsJsonObject().get("status").toString();
+            if (playerStatus.equals("in_action")){
+                bet = array.getAsJsonObject().get("bet").getAsInt();
+            }
+        }
         int currentBuyIn = json.get("current_buy_in").getAsInt();
-        return currentBuyIn;
+        return currentBuyIn-bet+json.get("minimum_raise").getAsInt();
     }
 
     public static void showdown(JsonElement game) {
