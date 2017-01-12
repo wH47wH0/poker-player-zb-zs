9dbaab28b60271c7a2a8e26a9ebb605e32307852
cickib <bellakocsis06@gmail.com>
2017-01-12 10:16:11 +0100

    current buyin

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,6 +1,7 @@
 package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.util.Map;
 
@@ -9,8 +10,9 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        System.out.println(request.getAsJsonObject().get("current_buy_in"));
-        return 1;
+        JsonObject json = request.getAsJsonObject();
+        int currentBuyIn = json.get("current_buy_in").getAsInt();
+        return currentBuyIn;
     }
 
     public static void showdown(JsonElement game) {
