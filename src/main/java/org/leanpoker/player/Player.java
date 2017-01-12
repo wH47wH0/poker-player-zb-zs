5ceab869cb61d89964fd9bb4c6412084f95df89d
cickib <bellakocsis06@gmail.com>
2017-01-12 10:51:24 +0100

    a

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -13,13 +13,13 @@ public class Player {
 
     public static int betRequest(JsonElement request) {
         JsonObject json = request.getAsJsonObject();
-        System.out.println("find me "+ json);
+        System.out.println("find me " + json);
         int bet = 0;
-        List holeCards = new ArrayList();
+        List<JsonObject> holeCards = new ArrayList();
         JsonArray jsonArray = json.get("players").getAsJsonArray();
         for (JsonElement array : jsonArray) {
             String playerStatus = array.getAsJsonObject().get("status").toString();
-            holeCards.add(array.getAsJsonObject().get("hole_cards"));
+            holeCards.add(array.getAsJsonObject().get("hole_cards").getAsJsonObject());
             if (playerStatus.equals("in_action")) {
                 bet = array.getAsJsonObject().get("bet").getAsInt();
             }
@@ -28,8 +28,15 @@ public class Player {
         System.out.println("find me " + (currentBuyIn - bet + json.get("minimum_raise").getAsInt()));
         for (Object o : holeCards) {
             System.out.println("find me " + o);
+            JsonObject js0 = holeCards.get(0).get("rank").getAsJsonObject();
+            JsonObject js1 = holeCards.get(1).get("rank").getAsJsonObject();
+            if (js0.equals(js1) || js0.equals("A") || js1.equals("A") || (js0.equals("K") && js1.equals("10") || js1.equals("K") && js0.equals("10"))) {
+                return currentBuyIn - bet + json.get("minimum_raise").getAsInt();
+
+            }
+
         }
-        return currentBuyIn - bet + json.get("minimum_raise").getAsInt();
+        return 0;
     }
 
     public static void showdown(JsonElement game) {
