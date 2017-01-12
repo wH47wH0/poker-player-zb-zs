78fe98ebc74b77371991398053c29899c890a928
cickib <bellakocsis06@gmail.com>
2017-01-12 10:58:11 +0100

    stack, buyin

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -16,10 +16,15 @@ public class Player {
     public static int betRequest(JsonElement request) {
         JsonObject json = request.getAsJsonObject();
         System.out.println("find me " + json);
+        String ownStack = "0";
         int bet = 0;
         List<JsonObject> holeCards = new ArrayList();
         JsonArray jsonArray = json.get("players").getAsJsonArray();
         for (JsonElement array : jsonArray) {
+            if (array.getAsJsonObject().get("player").toString().equals("_ZB.ZS")){
+                ownStack = array.getAsJsonObject().get("stack").toString();
+            }
+
             String playerStatus = array.getAsJsonObject().get("status").toString();
             holeCards.add(array.getAsJsonObject().get("hole_cards").getAsJsonObject());
             if (playerStatus.equals("in_action")) {
@@ -28,20 +33,24 @@ public class Player {
         }
         int currentBuyIn = json.get("current_buy_in").getAsInt();
         System.out.println("find me " + (currentBuyIn - bet + json.get("minimum_raise").getAsInt()));
-        if (holeCards.size() == 2){
+        if(currentBuyIn < (Integer.parseInt(ownStack)/3) ){
 
-            for (Object o : holeCards) {
-                System.out.println("find me " + o);
-                JsonObject js0 = holeCards.get(0).get("rank").getAsJsonObject();
-                JsonObject js1 = holeCards.get(1).get("rank").getAsJsonObject();
-                if (js0.equals(js1) || js0.equals("A") || js1.equals("A") || (js0.equals("K") && js1.equals("10") || js1.equals("K") && js0.equals("10"))) {
-                    return currentBuyIn - bet + json.get("minimum_raise").getAsInt();
+            if (holeCards.size() == 2){
+
+                for (Object o : holeCards) {
+                    System.out.println("find me " + o);
+                    JsonObject js0 = holeCards.get(0).get("rank").getAsJsonObject();
+                    JsonObject js1 = holeCards.get(1).get("rank").getAsJsonObject();
+                    if (js0.equals(js1) || js0.equals("A") || js1.equals("A") || (js0.equals("K") && js1.equals("10") || js1.equals("K") && js0.equals("10"))) {
+                        return currentBuyIn - bet + json.get("minimum_raise").getAsInt();
+
+                    }
 
                 }
-
             }
         }
         return 0;
+
     }
 
     public static void showdown(JsonElement game) {
