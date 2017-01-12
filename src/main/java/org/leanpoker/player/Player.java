04e9b32bb11094a2204142790328b0fe28b510eb
cickib <bellakocsis06@gmail.com>
2017-01-12 10:36:37 +0100

    hole cards in log

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -3,7 +3,9 @@ package org.leanpoker.player;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import jdk.nashorn.api.scripting.JSObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Player {
 
@@ -11,17 +13,22 @@ public class Player {
 
     public static int betRequest(JsonElement request) {
         JsonObject json = request.getAsJsonObject();
-        int bet=0;
+        int bet = 0;
+        List holeCards = new ArrayList();
         JsonArray jsonArray = json.get("players").getAsJsonArray();
-        for (JsonElement array : jsonArray){
+        for (JsonElement array : jsonArray) {
             String playerStatus = array.getAsJsonObject().get("status").toString();
-            if (playerStatus.equals("in_action")){
+            holeCards.add(array.getAsJsonObject().get("hole_cards"));
+            if (playerStatus.equals("in_action")) {
                 bet = array.getAsJsonObject().get("bet").getAsInt();
             }
         }
         int currentBuyIn = json.get("current_buy_in").getAsInt();
-        System.out.println("find me "+ (currentBuyIn-bet+json.get("minimum_raise").getAsInt()));
-        return currentBuyIn-bet+json.get("minimum_raise").getAsInt();
+        System.out.println("find me " + (currentBuyIn - bet + json.get("minimum_raise").getAsInt()));
+        for (Object o : holeCards) {
+            System.out.println("find me " + o);
+        }
+        return currentBuyIn - bet + json.get("minimum_raise").getAsInt();
     }
 
     public static void showdown(JsonElement game) {
